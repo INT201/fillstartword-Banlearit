e0c5eb7e5594acac68c50e62adc3b14c0f824208
Banlearit Siriboon <banlearit.ares@mail.kmutt.ac.th>
2022-09-01 14:06:50 +0700

    code : update

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,13 +2,15 @@ const {template} = require('@babel/core')
 
 function fillStartWord(startWord, word) {
     // code here
-    if (word == null && undefined){
+
+    if (word == null || undefined){
         return undefined
     }else if(startWord === word.slice(0,startWord.length)){
         return word;
     }else if (word){
         return startWord + word;
     }
+
 
 }
 
