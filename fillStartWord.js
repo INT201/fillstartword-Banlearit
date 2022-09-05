3486c66489cab31a35b736e919607c3302a07160
Banlearit Siriboon <banlearit.ares@mail.kmutt.ac.th>
2022-09-05 13:43:50 +0700

    test vscode to github

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,7 +2,7 @@ const {template} = require('@babel/core')
 
 function fillStartWord(startWord, word) {
     // code here
-    if (word == null || undefined){
+    if (word === null || undefined){
         return undefined
     }else if(startWord === word.slice(0,startWord.length)){
         return word;
@@ -12,6 +12,7 @@ function fillStartWord(startWord, word) {
 }
 // test vscode 
 // test again
+// test vscode1
 module.exports = fillStartWord
 
 
